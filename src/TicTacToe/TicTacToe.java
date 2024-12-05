6493847a4c489cafe2c02f19dec6e8e5bd0a1fc2
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 19:51:29 +0200

    TicTacToe 3 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,16 +1,28 @@
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        String[][] board = {
-                {"X", "O", "X"},
-                {"O", "X", "O"},
-                {"X", "X", "O"}
-        };
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
+        System.out.print("Enter cells: ");
+        String input = scanner.nextLine();
+
+        if (input.length() == 9 && input.matches("[XO_]+")) {
+            printBoard(input);
+        } else {
+            System.out.println("Invalid input!");
         }
+    }
+
+    public static void printBoard(String input) {
+        System.out.println("---------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(input.charAt(i * 3 + j) + " ");
+            }
+            System.out.println("|");
+        }
+        System.out.println("---------");
     }
 }
