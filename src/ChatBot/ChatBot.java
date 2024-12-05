9b65c6246b139772d113634ff89cbb313a702ee5
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 15:05:53 +0200

    ChatBot 3-rd stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -7,6 +7,16 @@ public class ChatBot {
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
+
+        scanner.close();
     }
 }
+
 
