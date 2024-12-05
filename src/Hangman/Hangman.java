f466c43929d4569b637a4b7d194e6616f198e58b
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 16:00:49 +0200

    Hangman stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,10 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] words = {"python", "java", "javascript", "kotlin"};
+        Random random = new Random();
 
-        String wordToGuess = "java";
+        String wordToGuess = words[random.nextInt(words.length)];
 
         System.out.println("HANGMAN");
         System.out.print("Guess the word: > ");
