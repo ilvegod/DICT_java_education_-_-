90667722dfee2fa74a720532310693b78772497e
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 15:45:40 +0200

    Hangman 3 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String wordToGuess = "java";
+
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        System.out.print("Guess the word: > ");
+        String userGuess = scanner.nextLine();
+
+        if (userGuess.equalsIgnoreCase(wordToGuess)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
     }
 }
