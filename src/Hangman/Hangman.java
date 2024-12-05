dd40e3e7c05c615e85f4d05175a799a0290d204e
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 15:39:42 +0200

    Hangman 2 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -1,4 +1,6 @@
-package Hangman;
-
 public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
 }
