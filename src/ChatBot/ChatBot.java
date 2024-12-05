3786f670ca664c5f49ae554a0b2279e1b9b3d734
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 15:17:55 +0200

    ChatBot 4-th stage

--- src/ChatBot/ChatBot.java
+++ src/ChatBot/ChatBot.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
-        String botName = "Bot1";
+        String botName = "Bot1D";
         int birthYear = 2024;
 
         System.out.println("Hello! My name is " + botName + ".");
@@ -11,12 +11,21 @@ public class ChatBot {
         System.out.println("Please, remind me your name.");
 
         Scanner scanner = new Scanner(System.in);
-        String userName = scanner.nextLine();
+        String yourName = scanner.nextLine();
 
-        System.out.println("What a great name you have, " + userName + "!");
+        System.out.println("What a great name you have, " + yourName + "!");
+
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
 
         scanner.close();
     }
 }
-
-
