023f3954f4413c546fabcba4747e8ff55ef763f7
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 19:49:26 +0200

    TicTacToe 2 stage

--- src/TicTacToe/TicTacToe.java
+++ src/TicTacToe/TicTacToe.java
@@ -1,4 +1,16 @@
-package TicTacToe;
-
 public class TicTacToe {
+    public static void main(String[] args) {
+        String[][] board = {
+                {"X", "O", "X"},
+                {"O", "X", "O"},
+                {"X", "X", "O"}
+        };
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
