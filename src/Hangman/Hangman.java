82b65640a428684d350f03bb799bbf3e71103169
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-05 16:05:31 +0200

    Hagman 6 stage

--- src/Hangman/Hangman.java
+++ src/Hangman/Hangman.java
@@ -8,16 +8,45 @@ public class Hangman {
         Random random = new Random();
 
         String wordToGuess = words[random.nextInt(words.length)];
-        String hint = wordToGuess.substring(0, 2) + "-".repeat(wordToGuess.length() - 2);
+        StringBuilder currentWord = new StringBuilder("-".repeat(wordToGuess.length()));
+        int attemptsLeft = 8;
+        String guessedLetters = "";
 
         System.out.println("HANGMAN");
-        System.out.println("Guess the word " + hint + ": > ");
-        String userGuess = scanner.nextLine();
+        System.out.println(currentWord);
 
-        if (userGuess.equalsIgnoreCase(wordToGuess)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+        while (attemptsLeft > 0) {
+            System.out.print("Input a letter: > ");
+            String userGuess = scanner.nextLine().toLowerCase();
+
+            if (guessedLetters.contains(userGuess)) {
+                System.out.println("You've already guessed that letter.");
+                continue;
+            }
+
+            guessedLetters += userGuess;
+
+            if (wordToGuess.contains(userGuess)) {
+                for (int i = 0; i < wordToGuess.length(); i++) {
+                    if (wordToGuess.charAt(i) == userGuess.charAt(0)) {
+                        currentWord.setCharAt(i, userGuess.charAt(0));
+                    }
+                }
+                System.out.println(currentWord);
+                if (currentWord.toString().equals(wordToGuess)) {
+                    System.out.println("You survived!");
+                    break;
+                }
+            } else {
+                attemptsLeft--;
+                System.out.println("That letter doesn't appear in the word");
+                System.out.println(currentWord);
+            }
+
+            if (attemptsLeft == 0) {
+                System.out.println("Thanks for playing!");
+                System.out.println("We'll see how well you did in the next stage");
+            }
         }
 
         scanner.close();
