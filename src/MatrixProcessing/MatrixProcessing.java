739be4a7a7ca7719a5b7ae8b55ee8c00f099ee5b
ilvegod <d.o.kaflik1205@gmail.com>
2024-12-06 15:10:24 +0200

    MP 3 stage

--- src/MatrixProcessing/MatrixProcessing.java
+++ src/MatrixProcessing/MatrixProcessing.java
@@ -4,55 +4,131 @@ public class MatrixProcessing {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
+        while (true) {
+            System.out.println("1. Add matrices");
+            System.out.println("2. Multiply matrix by a constant");
+            System.out.println("3. Multiply matrices");
+            System.out.println("0. Exit");
+            System.out.print("Your choice: ");
+            int choice = scanner.nextInt();
 
-        int[][] matrixA = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrixA[i][j] = scanner.nextInt();
+            if (choice == 0) {
+                break;
             }
-        }
 
-        int[][] matrixB = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrixB[i][j] = scanner.nextInt();
-            }
-        }
+            if (choice == 1) {
+                System.out.print("Enter size of first matrix: ");
+                int n1 = scanner.nextInt();
+                int m1 = scanner.nextInt();
+                int[][] matrixA = new int[n1][m1];
+                System.out.println("Enter first matrix:");
+                for (int i = 0; i < n1; i++) {
+                    for (int j = 0; j < m1; j++) {
+                        matrixA[i][j] = scanner.nextInt();
+                    }
+                }
 
-        if (n == matrixB.length && m == matrixB[0].length) {
-            int[][] sumMatrix = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    sumMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+                System.out.print("Enter size of second matrix: ");
+                int n2 = scanner.nextInt();
+                int m2 = scanner.nextInt();
+                int[][] matrixB = new int[n2][m2];
+                System.out.println("Enter second matrix:");
+                for (int i = 0; i < n2; i++) {
+                    for (int j = 0; j < m2; j++) {
+                        matrixB[i][j] = scanner.nextInt();
+                    }
+                }
+
+                if (n1 == n2 && m1 == m2) {
+                    int[][] sumMatrix = new int[n1][m1];
+                    for (int i = 0; i < n1; i++) {
+                        for (int j = 0; j < m1; j++) {
+                            sumMatrix[i][j] = matrixA[i][j] + matrixB[i][j];
+                        }
+                    }
+
+                    System.out.println("The result is:");
+                    for (int i = 0; i < n1; i++) {
+                        for (int j = 0; j < m1; j++) {
+                            System.out.print(sumMatrix[i][j] + " ");
+                        }
+                        System.out.println();
+                    }
+                } else {
+                    System.out.println("The operation cannot be performed.");
                 }
             }
 
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < m; j++) {
-                    System.out.print(sumMatrix[i][j] + " ");
+            if (choice == 2) {
+                System.out.print("Enter size of matrix: ");
+                int n = scanner.nextInt();
+                int m = scanner.nextInt();
+                double[][] matrix = new double[n][m];
+                System.out.println("Enter matrix:");
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        matrix[i][j] = scanner.nextDouble();
+                    }
                 }
-                System.out.println();
+
+                System.out.print("Enter constant: ");
+                double constant = scanner.nextDouble();
+
+                System.out.println("The result is:");
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < m; j++) {
+                        System.out.print(matrix[i][j] * constant + " ");
+                    }
+                    System.out.println();
+                }
             }
-        } else {
-            System.out.println("ERROR");
+
+            if (choice == 3) {
+                System.out.print("Enter size of first matrix: ");
+                int n1 = scanner.nextInt();
+                int m1 = scanner.nextInt();
+                int[][] matrixA = new int[n1][m1];
+                System.out.println("Enter first matrix:");
+                for (int i = 0; i < n1; i++) {
+                    for (int j = 0; j < m1; j++) {
+                        matrixA[i][j] = scanner.nextInt();
+                    }
+                }
+
+                System.out.print("Enter size of second matrix: ");
+                int n2 = scanner.nextInt();
+                int m2 = scanner.nextInt();
+                if (m1 != n2) {
+                    System.out.println("The operation cannot be performed.");
+                    continue;
+                }
+                int[][] matrixB = new int[n2][m2];
+                System.out.println("Enter second matrix:");
+                for (int i = 0; i < n2; i++) {
+                    for (int j = 0; j < m2; j++) {
+                        matrixB[i][j] = scanner.nextInt();
+                    }
+                }
+
+                int[][] result = new int[n1][m2];
+                for (int i = 0; i < n1; i++) {
+                    for (int j = 0; j < m2; j++) {
+                        for (int k = 0; k < m1; k++) {
+                            result[i][j] += matrixA[i][k] * matrixB[k][j];
+                        }
+                    }
+                }
+
+                System.out.println("The result is:");
+                for (int i = 0; i < n1; i++) {
+                    for (int j = 0; j < m2; j++) {
+                        System.out.print(result[i][j] + " ");
+                    }
+                    System.out.println();
+                }
+            }
         }
 
-        int constant = scanner.nextInt();
-
-        int[][] matrix = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
-        }
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                System.out.print(matrix[i][j] * constant + " ");
-            }
-            System.out.println();
-        }
+        scanner.close();
     }
 }
